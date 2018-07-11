7b35471153a05af716967f6a4261112c392cfbac
mikeyyg96 <gallegom58@gmail.com>
2018-07-11 14:49:53 -0400

    - User Friendly - Start/Stop Feature - Random Interval Range

--- smart_clicker.py
+++ smart_clicker.py
@@ -9,6 +9,7 @@
 '''
 
 from tkinter import *
+from tkinter import messagebox
 import threading
 import numpy as np
 import random
@@ -18,8 +19,8 @@ import pyautogui as ms
 
 # Initialize the window
 window = Tk()
-window.title("Smart Clicker Version 0.1.0")
-window.geometry('800x400')
+window.title("Smart Clicker Version 0.2.0")
+window.geometry('800x300')
 
 # Controller object to get mouse position
 mouse = Controller()
@@ -32,9 +33,15 @@ mouse_variable_y = StringVar()
 label_set_location = StringVar()
 string_set_location = StringVar()
 directions = StringVar()
+number_of_clicks = StringVar()
+interval_range = StringVar()
+input_clicks = StringVar()
 
 # Placeholder spread of cursor values
 targeted_mouse_area = None
+
+# For click thread
+clicks_running = False
 
 # Mouse position will be set from the user when the user presses f11
 mouse_position_x = 0
@@ -44,20 +51,45 @@ mouse_position_y = 0
 directions_label = Label(window, textvariable=directions).place(x=400, y=15, anchor="center")
 label_placeholder = Label(window, text="").grid(row=0, column=0)
 
-label_x = Label(window, textvariable=string_variable_x).grid(row=1, column=0)
-mouse_x = Label(window, textvariable=mouse_variable_x).grid(row=1, column=1)
+# label_x = Label(window, textvariable=string_variable_x).grid(row=1, column=0)
+# mouse_x = Label(window, textvariable=mouse_variable_x).grid(row=1, column=1)
+#
+# label_y = Label(window, textvariable=string_variable_y).grid(row=2, column=0)
+# mouse_y = Label(window, textvariable=mouse_variable_y).grid(row=2, column=1)
+#
+# display_location = Label(window, textvariable=label_set_location).grid(row=3, column=0)
+# set_location = Label(window, textvariable=string_set_location).grid(row=3, column=1)
 
-label_y = Label(window, textvariable=string_variable_y).grid(row=2, column=0)
-mouse_y = Label(window, textvariable=mouse_variable_y).grid(row=2, column=1)
+label_x = Label(window, textvariable=string_variable_x).place(x=350, y=45, anchor="center")
+mouse_x = Label(window, textvariable=mouse_variable_x).place(x=450, y=45, anchor="center")
 
-display_location = Label(window, textvariable=label_set_location).grid(row=3, column=0)
-set_location = Label(window, textvariable=string_set_location).grid(row=3, column=1)
+label_y = Label(window, textvariable=string_variable_y).place(x=350, y=75, anchor="center")
+mouse_y = Label(window, textvariable=mouse_variable_y).place(x=450, y=75, anchor="center")
+
+display_clicks = Label(window, textvariable=number_of_clicks).place(x=350, y=105, anchor="center")
+input_clicks = Entry(window, width=5)
+input_clicks.insert(END, '1')
+input_clicks.place(x=450, y=105, anchor="center")
+
+display_interval = Label(window, textvariable=interval_range).place(x=350, y=135, anchor="center")
+input_interval_x = Entry(window, width=3)
+input_interval_x.insert(END, '.3')
+input_interval_x.place(x=435, y=135, anchor="center")
+dash_interval = Label(window, text="-").place(x=460, y=135, anchor="center")
+input_interval_y = Entry(window, width=3)
+input_interval_y.insert(END, '.7')
+input_interval_y.place(x=485, y=135, anchor="center")
+
+display_location = Label(window, textvariable=label_set_location).place(x=350, y=165, anchor="center")
+set_location = Label(window, textvariable=string_set_location).place(x=450, y=165, anchor="center")
 
 string_variable_x.set('Mouse X: ')
 string_variable_y.set('Mouse Y: ')
-label_set_location.set('Set (f11): ')
+label_set_location.set('Click location (f11): ')
+number_of_clicks.set('Number of clicks: ')
+interval_range.set('Interval Range: ')
 
-directions.set('Press f11 to set location, press f12 to start clicker')
+directions.set('press f11 to start clicker at cursor and f12 to stop clicker')
 
 
 # This function returns the mouse coordinates of user's cursor during runtime
@@ -100,25 +132,73 @@ def set_mouse_area(position):
 
 def background():
     def on_press(key):
-        if key == keyboard.Key.f11:
+        global clicks_running
 
+        if key == keyboard.Key.f11 and not clicks_running:
             # Passing the position of user's cursor to the function set_mouse_area
             print('Mouse position set at: {}'.format(mouse.position))
             position = mouse.position
             string_set_location.set('x = ' + str(position[0]) + ',' + ' y = ' + str(position[1]))
             set_mouse_area(position)
 
-        if key == keyboard.Key.f12:
+            t = threading.Thread(target=start_clicks)
+            t.start()
+            # catching value exceptions when the user puts in wrong values in the GUI
 
-                # Clicks for 10 seconds at 10 randomly differently locations in the targeted area
-                for x in range(10):
-                    coordinates = targeted_mouse_area[random.randint(0, 20), random.randint(0, 20)]
-                    ms.click(coordinates[0], coordinates[1], clicks=1, interval=random.uniform(0.0, 1.0), button='left')
-                    print('clicked at {}'.format(coordinates))
+        if key == keyboard.Key.f12:
+            clicks_running = False
 
     # Listens for keyboard inputs [Do not touch]
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
+
+
+def start_clicks():
+    global clicks_running
+    clicks_running = True
+
+    try:
+        if float(input_interval_x.get()) < 0:
+            interval_x = 0.0
+        else:
+            interval_x = float(input_interval_x.get())
+    except ValueError:
+        interval_x = 0
+
+    try:
+        if float(input_interval_y.get()) < 0:
+            interval_y = 0.0
+        else:
+            interval_y = float(input_interval_y.get())
+    except ValueError:
+        interval_y = 0
+
+    try:
+        if int(input_clicks.get()) > 0:
+            clicks = int(input_clicks.get())
+        else:
+            clicks = 1
+    except ValueError:
+        clicks = 1
+
+    if interval_x > interval_y:
+        interval_x, interval_y = interval_y, interval_x
+
+    # Clicks for 10 seconds at 10 randomly differently locations in the targeted area
+    for x in range(clicks):
+
+        if not clicks_running:
+            break
+
+        try:
+            coordinates = targeted_mouse_area[random.randint(0, 20), random.randint(0, 20)]
+        except TypeError:
+            coordinates = (1, 1)
+        ms.click(coordinates[0], coordinates[1], clicks=1, interval=random.uniform(interval_x, interval_y),
+                 button='left', pause=0.1)
+        print('clicked at {}'.format(coordinates))
+
+    clicks_running = False
 
 
 # This thread will run the listener in the background for optimal performance
@@ -131,4 +211,5 @@ window.resizable(False, False)
 # Will update every time the cursor is moved
 set_mouse_label()
 
+Button(window, text="QUIT", command=window.destroy)
 window.mainloop()
